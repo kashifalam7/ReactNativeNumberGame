d3c0d974b850a5f55f96f50182971cf030990d90
Kashif Jamshed <talktokashiif@gmail.com>
2023-04-14 16:44:01 +0530

    Game screen updated

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,10 @@
 import { Text, View,StyleSheet, SafeAreaView } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen () {
  return(
  <View style={styles.screen}>
-  <Text> Oponnent's Guess</Text>
-
+  <Title>Oponent's title</Title>
 <View>
     <Text>Higher or Lower?</Text>
 </View>
@@ -21,6 +21,15 @@ const styles = StyleSheet.create({
     screen:{
         flex:1,
         padding:40,
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+        textAlign:'center',
+        color:'yellow',
+        borderWidth:2,
+        borderColor:'yellow' 
+        
     }
 });
 export default GameScreen;
